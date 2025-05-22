46382b52fd2f66c7a604f75a0cf949c93b9431e6
Naoki Mita <naoki.mita18@gmail.com>
2025-05-22 13:13:13 -0700

    add navlinks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useState } from "react";
+import { builder, BuilderComponent } from "@builder.io/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,7 +14,19 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function Home() {
+builder.init(process.env.NEXT_PUBLIC_BUILDER_PUBLIC_API_KEY || "");
+
+export async function getServerSideProps() {
+  const template =
+    (await builder
+      .get("navigation-links-test", { options: { enrich: true } })
+      .toPromise()) || null;
+
+  return { props: { template } };
+}
+
+export default function Home({ template }: any) {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <>
       <Head>
@@ -26,91 +39,25 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol>
-            <li>
-              Get started by editing <code>src/pages/index.tsx</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+          <div
+            className={styles.modal}
+            onMouseOver={() => setOpenModal(true)}
+            onMouseLeave={() => setOpenModal(false)}
+          >
+            <div
+              className={styles.modal}
+              style={{ display: openModal ? "block" : "none" }}
             >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
+              <h1>Modal Title</h1>
+              <BuilderComponent
+                model="navigation-links-test"
+                content={template}
+                options={{ enrich: true }}
               />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
+            </div>
+            <button>Open Modal</button>
           </div>
         </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
       </div>
     </>
   );
